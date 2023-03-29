4b9ec06866556738eeadfd4e6412391e76ed60ec
Patryk Wiśniewski <patrickwisniewski91@gmail.com>
2023-03-29 03:21:12 +0200

    End of section 7 - not working

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import 'semantic-ui-css/semantic.min.css'
 import './app/layout/styles.css';
 import App from './app/layout/App';
 import reportWebVitals from './reportWebVitals';
+import { store, StoreContext } from './app/stores/store';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <App />
+// const container = document.getElementById('root') as HTMLElement;
+// const root = createRoot(container);
+ReactDOM.render(
+  <StoreContext.Provider value={store}>
+    <App />
+  </StoreContext.Provider>,
+  //document.getElementById('root') as HTMLElement
+  document.body
 );
 
 // If you want to start measuring performance in your app, pass a function
